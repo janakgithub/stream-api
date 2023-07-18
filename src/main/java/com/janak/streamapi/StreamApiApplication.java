cac4b7b525a65928daae1d97be7dc39212173501
janakgithub <janak4u@gmail.com>
2023-07-18 13:21:49 +0930

    Learning Streams in Java

---
+++ src/main/java/com/janak/streamapi/StreamApiApplication.java
@@ -0,0 +1,52 @@
+package com.janak.streamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class StreamApiApplication {
+
+    static List<Employee> employees = new ArrayList<>();
+    static {
+        employees.add(new Employee("janak","adhikari",4000.0, List.of("Project1","Project2")));
+        employees.add(new Employee("Niki","adhikari",5000.0, List.of("Project3","Project4")));
+        employees.add(new Employee("Jiaa","adhikari",6000.0, List.of("Project5","Project6")));
+    }
+
+
+    public static void main(String[] args) {
+        //SpringApplication.run(StreamApiApplication.class, args);
+
+        //foreach - terminal operation
+        employees.stream()
+                .forEach(employee -> System.out.println(employee));
+
+
+        //map
+        //collect
+        System.out.println("**********map and collect operation ************");
+         List<Employee> increasedSalary = employees.stream()
+                .map(employee -> new Employee(
+                        employee.getFirstName(),
+                        employee.getLastName(),
+                        employee.getSalary() * 1.10,
+                        employee.getProjects()
+                )).collect(Collectors.toList());
+        System.out.println(increasedSalary);
+
+        //filter
+        System.out.println("**********filter operation ************");
+        List<Employee> employeeList = employees.stream()
+                .filter(employee -> employee.getSalary() >= 4000.0)
+                .map(employee -> new Employee(employee.getFirstName(), employee.getLastName(), employee.getSalary(), employee.getProjects()))
+                .collect(Collectors.toList());
+        System.out.println(employeeList);
+
+    }
+
+}
